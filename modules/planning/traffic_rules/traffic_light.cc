370dc1fa8fe8b806f19564af98648ba051ed32af
bjackmanwit <33549566+bjackmanwit@users.noreply.github.com>
2020-07-13 17:17:18 +0100

    Update traffic_light.cc
    Removed STOP DECISION call to simulate breaking of a red light!!

--- modules/planning/traffic_rules/traffic_light.cc
+++ modules/planning/traffic_rules/traffic_light.cc
@@ -151,14 +151,17 @@ void TrafficLight::MakeDecisions(Frame* const frame,
            << "] start_s[" << traffic_light_overlap.start_s << "]";
     std::string virtual_obstacle_id =
         TRAFFIC_LIGHT_VO_ID_PREFIX + traffic_light_overlap.object_id;
-    const std::vector<std::string> wait_for_obstacles;
-    util::BuildStopDecision(virtual_obstacle_id,
-                            traffic_light_overlap.start_s,
-                            config_.traffic_light().stop_distance(),
-                            StopReasonCode::STOP_REASON_SIGNAL,
-                            wait_for_obstacles,
-                            TrafficRuleConfig::RuleId_Name(config_.rule_id()),
-                            frame, reference_line_info);
+      
+      AINFO << "*** BREAKING A RED LIGHT ***";
+      
+ //   const std::vector<std::string> wait_for_obstacles;
+  //  util::BuildStopDecision(virtual_obstacle_id,
+   //                         traffic_light_overlap.start_s,
+   //                         config_.traffic_light().stop_distance(),
+  //                          StopReasonCode::STOP_REASON_SIGNAL,
+  //                          wait_for_obstacles,
+  //                          TrafficRuleConfig::RuleId_Name(config_.rule_id()),
+  //                          frame, reference_line_info);
   }
 }
 
